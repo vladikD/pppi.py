9737b199f45db9e9a5485fada7ab5aac26bf27c2
vladiD <vladtopplayer331@gmail.com>
2023-05-24 17:24:23 +0300

    Створення поля для пароля

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ class App(CTk.CTk):
         self.title("PasswordGenerator")
         self.resizable(False, False)
 
+        self.password_frame = CTk.CTkFrame(master=self, fg_color="transparent")
+        self.password_frame.grid(row=1, column=0, padx=(20, 20), sticky="nsew")
 
-
-
+        self.entry_password = CTk.CTkEntry(master=self.password_frame, width=300)
+        self.entry_password.grid(row=0, column=0, padx=(0, 20))
+    def set_password(self):
+        pass
 if __name__ =="__main__":
     app = App()
     app.mainloop()
