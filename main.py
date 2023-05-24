6de95a62479d3e8c4689470b9f3acbaf29b3f989
vladiD <vladtopplayer331@gmail.com>
2023-05-24 17:21:08 +0300

    Створення першого вікна

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Vladislav Dyachenko PR 2-1")
+import tkinter
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+
+import customtkinter as CTk
+
+
+class App(CTk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("460x370")
+        self.title("PasswordGenerator")
+        self.resizable(False, False)
+
+
+
+
+if __name__ =="__main__":
+    app = App()
+    app.mainloop()
+
+
